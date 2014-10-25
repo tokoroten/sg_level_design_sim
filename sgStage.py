3cbb351fa129d50956268a004e1d08f163b55876
tokoroten <shinta.nakayama+github@gmail.com>
2014-10-25 20:22:13 +0900

    add round value to stat output

--- sgStage.py
+++ sgStage.py
@@ -7,14 +7,15 @@ class sgStage:
         self.players = []
         self.stages_difficult = []
         self.stages_result = []
+        self.stat_round_num = 5
 
     def create_player(self):
-        for i in xrange(100):
+        for i in xrange(10000):
             self.players.append(sgPlayer.sgPlayer())
 
     def create_stage(self):
         for i in xrange(100):
-            self.stages_difficult.append(6 + i + random.randint(1,10))
+            self.stages_difficult.append(10 + i * 5 + random.randint(1,10))
             self.stages_result.append({})
 
     def simulate(self):
@@ -26,7 +27,7 @@ class sgStage:
             stage_level = self.stages_difficult[stage_id]
             result = player.try_stage(stage_level)
 
-            player_power = player.charactor_power
+            player_power = int(player.charactor_power / self.stat_round_num) * self.stat_round_num
 
             if player_power not in self.stages_result[stage_id]:
                 self.stages_result[stage_id][player_power] = [0, 0]
@@ -46,6 +47,7 @@ class sgStage:
     def get_user_average_status(self):
         total_power = 0
         active_count = 0
+
         for player in self.players:
             if player.is_active:
                 active_count += 1
@@ -61,14 +63,17 @@ class sgStage:
 
         fp = open('stat_result.csv', "w")
 
-        header = ["stage_id"]
-        for i in xrange(max_power_range):
-            header.append("status_%.4d" % i)
+        header = ["stage_id", "drop_user_num"]
+        for charactor_power in xrange(0, max_power_range, self.stat_round_num):
+            header.append("status_%.4d" % charactor_power)
         print >>fp, ",".join(header)
 
         for stage_id in xrange(len(self.stages_result)):
-            out = [stage_id]
-            for charactor_power in xrange(max_power_range):
+            out = ["stage_id_%.4d" % stage_id]
+            drop_user_num = [player.stage_counter for player in self.players if player.is_active == False].count(stage_id)
+            out.append(drop_user_num)
+
+            for charactor_power in xrange(0, max_power_range, self.stat_round_num):
                 if charactor_power in self.stages_result[stage_id]:
                     win = self.stages_result[stage_id][charactor_power][1]
                     lose =  self.stages_result[stage_id][charactor_power][0]
@@ -88,7 +93,7 @@ if __name__ == "__main__":
     stage.create_stage()
     stage.create_player()
 
-    for i in xrange(50):
+    for i in xrange(100):
         stage.simulate()
         active_user = stage.get_active_user_count()
         average_power = stage.get_user_average_status()
